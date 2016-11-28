5f2f1020b09f6d40e2c7296f9e19595a76d8e657
Frédéric Guillot <fguillot@internap.com>
2016-11-28 09:46:42 -0500

    Update devstack plugin
    Change-Id: Ie925b4481c865f398ac29eadfc58e9bfb18604c9

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -27,29 +27,16 @@ function _install_mongodb {
 
 function almanach_configure {
     sudo install -d -o $STACK_USER -m 755 $ALMANACH_CONF_DIR
-    cp ${ALMANACH_DIR}/almanach/resources/config/almanach.cfg $ALMANACH_CONF
 
-    iniset $ALMANACH_CONF ALMANACH auth_token secret
-    iniset $ALMANACH_CONF ALMANACH auth_strategy private_key
-    iniset $ALMANACH_CONF ALMANACH volume_existence_threshold 60
-    iniset $ALMANACH_CONF ALMANACH device_metadata_whitelist metering.billing_mode
+    iniset $ALMANACH_CONF DEFAULT debug "True"
 
-    iniset $ALMANACH_CONF MONGODB url mongodb://localhost/almanach
-    iniset $ALMANACH_CONF MONGODB database almanach
-    iniset $ALMANACH_CONF MONGODB indexes project_id,start,end
+    iniset $ALMANACH_CONF api bind_ip $ALMANACH_SERVICE_HOST
+    iniset $ALMANACH_CONF api bind_port $ALMANACH_SERVICE_PORT
 
-    iniset $ALMANACH_CONF RABBITMQ url amqp://stackrabbit:secret@localhost:5672
-    iniset $ALMANACH_CONF RABBITMQ indexes project_id,start,end
-    iniset $ALMANACH_CONF RABBITMQ queue almanach.info
-    iniset $ALMANACH_CONF RABBITMQ exchange almanach.info
-    iniset $ALMANACH_CONF RABBITMQ routing.key almanach.info
-    iniset $ALMANACH_CONF RABBITMQ retry.time.to.live 10
-    iniset $ALMANACH_CONF RABBITMQ retry.exchange almanach.retry
-    iniset $ALMANACH_CONF RABBITMQ retry.maximum 3
-    iniset $ALMANACH_CONF RABBITMQ retry.queue almanach.retry
-    iniset $ALMANACH_CONF RABBITMQ retry.return.exchange almanach
-    iniset $ALMANACH_CONF RABBITMQ dead.queue almanach.dead
-    iniset $ALMANACH_CONF RABBITMQ dead.exchange almanach.dead
+    iniset $ALMANACH_CONF auth auth_token secret
+    iniset $ALMANACH_CONF auth auth_strategy private_key
+
+    iniset $ALMANACH_CONF collector transport_url rabbit://stackrabbit:secret@localhost:5672
 }
 
 # Create almanach related accounts in Keystone
@@ -78,12 +65,14 @@ function install_almanach {
 }
 
 function start_almanach {
-    run_process almanach-collector "$ALMANACH_BIN_DIR/almanach collector $ALMANACH_CONF"
-    run_process almanach-api "$ALMANACH_BIN_DIR/almanach api $ALMANACH_CONF --host 0.0.0.0"
+    run_process almanach-collector "$ALMANACH_BIN_DIR/almanach-collector --config-file $ALMANACH_CONF"
+    run_process almanach-api "$ALMANACH_BIN_DIR/almanach-api --config-file $ALMANACH_CONF"
 }
 
 function stop_almanach {
-    echo "todo"
+    for serv in almanach-api almanach-collector; do
+        stop_process $serv
+    done
 }
 
 ALMANACH_BIN_DIR=$(get_python_exec_prefix)
