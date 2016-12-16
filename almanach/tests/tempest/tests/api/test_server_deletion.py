dfba7fcdc2f4815022e1038d5e1fe42a39e2880d
Marc Aubry <maubry@internap.com>
2016-12-16 00:40:19 -0500

    Add Tempest scenario for API
    Change-Id: If1c38b1a2004295675a7c66722d79ce80dec8dcf

---
+++ almanach/tests/tempest/tests/api/test_server_deletion.py
@@ -0,0 +1,42 @@
+# Copyright 2016 Internap.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from uuid import uuid4
+
+from almanach.tests.tempest.tests.api import base
+import oslo_serialization
+
+
+class TestServerDeletion(base.BaseAlmanachTest):
+    def setUp(self):
+        super(base.BaseAlmanachTest, self).setUp()
+
+    @classmethod
+    def resource_setup(cls):
+        super(TestServerDeletion, cls).resource_setup()
+
+    def test_instance_delete(self):
+        tenant_id = str(uuid4())
+        server = self.get_server_creation_payload()
+        self.create_server_through_api(tenant_id, server)
+
+        self.almanach_client.delete_server(server['id'],
+                                              oslo_serialization.jsonutils.dumps({'date': '2016-01-01T19:50:00Z'}))
+        _, response_body = self.almanach_client.get_tenant_entities(tenant_id)
+
+        response_body = oslo_serialization.jsonutils.loads(response_body)
+        self.assertIsInstance(response_body, list)
+        self.assertEqual(1, len(response_body))
+
+        self.assertEqual(server['id'], response_body[0]['entity_id'])
+        self.assertIsNotNone(response_body[0]['end'])
---
+++ almanach/tests/tempest/tests/api/test_server_update.py
@@ -0,0 +1,40 @@
+# Copyright 2016 Internap.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from uuid import uuid4
+
+from almanach.tests.tempest.tests.api import base
+import oslo_serialization
+
+
+class TestServerUpdate(base.BaseAlmanachTest):
+    def setUp(self):
+        super(base.BaseAlmanachTest, self).setUp()
+
+    @classmethod
+    def resource_setup(cls):
+        super(TestServerUpdate, cls).resource_setup()
+
+    def test_instance_update(self):
+        server = self.get_server_creation_payload()
+        self.create_server_through_api(str(uuid4()), server)
+
+        update_field = oslo_serialization.jsonutils.dumps({'start_date': '2016-04-14T18:30:00.00Z',
+                                                           'flavor': 'NewFlavor'})
+        resp, response_body = self.almanach_client_().update_server(server['id'], update_field)
+
+        updated_server = oslo_serialization.jsonutils.loads(response_body)
+
+        self.assertEqual(server['id'], updated_server['entity_id'])
+        self.assertEqual('2016-04-14 18:30:00+00:00', updated_server['start'])
+        self.assertEqual('NewFlavor', updated_server['flavor'])
