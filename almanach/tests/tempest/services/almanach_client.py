ef8897f58bc959505be105c499d0bfbe5e872cd8
Frédéric Guillot <fguillot@internap.com>
2017-05-12 13:28:10 -0400

    Add API versioning
    - All OpenStack projects have API versioning
    - Existing endpoints are now prefixed with /v1
    - Still fully backward compatible with old endpoints
    - No HTTP redirects is used to avoid unexpected behaviors with
    existing clients
    
    Change-Id: If51f3291c44615991b3378b711dffacc1bd2591f

--- almanach/tests/tempest/services/almanach_client.py
+++ almanach/tests/tempest/services/almanach_client.py
@@ -19,6 +19,7 @@ CONF = config.CONF
 
 
 class AlmanachClient(rest_client.RestClient):
+    api_version = 'v1'
 
     def __init__(self, auth_provider):
         super(AlmanachClient, self).__init__(
@@ -32,11 +33,11 @@ class AlmanachClient(rest_client.RestClient):
         return resp, response_body
 
     def create_server(self, tenant_id, body):
-        resp, response_body = self.post('/project/{}/instance'.format(tenant_id), body=body)
+        resp, response_body = self.post('project/{}/instance'.format(tenant_id), body=body)
         return resp, response_body
 
     def delete_server(self, instance_id, body):
-        resp, response_body = self.delete('/instance/{}'.format(instance_id), body=body)
+        resp, response_body = self.delete('instance/{}'.format(instance_id), body=body)
         return resp, response_body
 
     def get_volume_type(self, volume_type_id):
@@ -44,28 +45,28 @@ class AlmanachClient(rest_client.RestClient):
         return resp, response_body
 
     def create_volume_type(self, body):
-        resp, response_body = self.post('/volume_type', body)
+        resp, response_body = self.post('volume_type', body)
         return resp, response_body
 
     def create_volume(self, tenant_id, body):
-        url = '/project/{}/volume'.format(tenant_id)
+        url = 'project/{}/volume'.format(tenant_id)
         resp, response_body = self.post(url, body)
         return resp, response_body
 
     def delete_volume(self, volume_id, body):
-        resp, response_body = self.delete('/volume/{}'.format(volume_id), body=body)
+        resp, response_body = self.delete('volume/{}'.format(volume_id), body=body)
         return resp, response_body
 
     def resize_volume(self, volume_id, body):
-        resp, response_body = self.put('/volume/{}/resize'.format(volume_id), body)
+        resp, response_body = self.put('volume/{}/resize'.format(volume_id), body)
         return resp, response_body
 
     def attach_volume(self, volume_id, body):
-        resp, response_body = self.put('/volume/{}/attach'.format(volume_id), body)
+        resp, response_body = self.put('volume/{}/attach'.format(volume_id), body)
         return resp, response_body
 
     def detach_volume(self, volume_id, body):
-        resp, response_body = self.put('/volume/{}/detach'.format(volume_id), body)
+        resp, response_body = self.put('volume/{}/detach'.format(volume_id), body)
         return resp, response_body
 
     def get_tenant_entities(self, tenant_id):
@@ -74,16 +75,16 @@ class AlmanachClient(rest_client.RestClient):
         return resp, response_body
 
     def update_server(self, instance_id, body):
-        url = '/entity/instance/{}'.format(instance_id)
+        url = 'entity/instance/{}'.format(instance_id)
         resp, response_body = self.put(url, body)
         return resp, response_body
 
     def rebuild(self, instance_id, body):
-        update_instance_rebuild_query = "/instance/{}/rebuild".format(instance_id)
+        update_instance_rebuild_query = "instance/{}/rebuild".format(instance_id)
         resp, response_body = self.put(update_instance_rebuild_query, body)
         return resp, response_body
 
     def resize(self, instance_id, body):
-        url = "/instance/{}/resize".format(instance_id)
+        url = "instance/{}/resize".format(instance_id)
         resp, response_body = self.put(url, body)
         return resp, response_body
